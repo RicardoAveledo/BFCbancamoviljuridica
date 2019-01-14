6b599f98fe2dc30f8026b5d5ea4e44f6711fb9a9
Aveledo <Ricardo Aveledo@fondocomun.com.ve>
2019-01-14 10:46:32 -0400

    Copiando los archivos para estar a la par con dev, master

--- bancamoviljuridica/src/pages/login/login.ts
+++ bancamoviljuridica/src/pages/login/login.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Toast } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 import { PosiciNConsolidadaPage } from '../posici-nconsolidada/posici-nconsolidada';
 import { DetalleDeLaCuentaPage } from '../detalle-de-la-cuenta/detalle-de-la-cuenta';
 import { DetalleDeTarjetaPage } from '../detalle-de-tarjeta/detalle-de-tarjeta';
 import { WelcomePage } from '../welcome/welcome';
+import { LoginProvider } from '../../providers/login/login';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'page-login',
@@ -11,10 +15,37 @@ import { WelcomePage } from '../welcome/welcome';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,  private formBuilder: FormBuilder, private toastCtrl: ToastController) {
   }
+
+//Mensaje al usuario al no introducir su nombre o contraseña
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Por favor, introduzca su nombre de usuario y clave para continuar',
+      duration: 3000,
+      position: 'botton'
+  });
+  toast.onDidDismiss(() => {
+    console.log('Dismissed toast');
+  });
+  toast.present();
+}
+
   goToWelcome(params){
     if (!params) params = {};
-    this.navCtrl.setRoot(WelcomePage);
+      if (this.credentialsForm.valid)
+      {
+       this.navCtrl.setRoot(WelcomePage);
+      }
+      else{
+     this.presentToast();
+      }
   }
+
+//Forma donde se valida el nombre y la contraseña del usuario
+  credentialsForm = this.formBuilder.group({
+      name: ['',Validators.required],
+      password:   ['', [Validators.required, Validators.minLength(5)]]
+    });
+
 }
