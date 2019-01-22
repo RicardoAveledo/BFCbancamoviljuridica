46aeee2d51d5a74d865bc2a7f0fd9e0dbbdd5ddb
Aveledo <Jonathan Aveledo@fondocomun.com.ve>
2019-01-22 11:11:20 -0400

    http post

--- bancamoviljuridica/src/pages/login/login.ts
+++ bancamoviljuridica/src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, Toast } from 'ionic-angular';
+import { NavController, Toast, GESTURE_REFRESHER } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { PosiciNConsolidadaPage } from '../posici-nconsolidada/posici-nconsolidada';
 import { DetalleDeLaCuentaPage } from '../detalle-de-la-cuenta/detalle-de-la-cuenta';
@@ -7,6 +7,14 @@ import { DetalleDeTarjetaPage } from '../detalle-de-tarjeta/detalle-de-tarjeta';
 import { WelcomePage } from '../welcome/welcome';
 import { LoginProvider } from '../../providers/login/login';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import 'rxjs/add/operator/map';
+import xml2js from 'xml2js';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Http, Headers, RequestOptions } from '@angular/http';
+
+
 
 
 @Component({
@@ -15,18 +23,56 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController,  private formBuilder: FormBuilder, private toastCtrl: ToastController) {
+  public xmlItems : any;
+  user:string;
+
+  constructor(public navCtrl: NavController,  private formBuilder: FormBuilder, 
+    private toastCtrl: ToastController, public LoginProvider: LoginProvider,public httpClient: HttpClient ) {
+
+      this.sendPostRequest();
   }
+
+
+
+  sendPostRequest() {
+    var headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/xml');
+    headers.append('Accept', 'application/xml');
+    const httpOptions = {
+      headers: new HttpHeaders({
+      'Content-Type':  'application/xml',
+      //'Authorization': 'my-auth-token'
+     })
+  };
+    //var requestOptions = new RequestOptions({headers:headers});
+
+    let postData = {
+      sAF_NombreUsuarioIn: "kilate",
+      //sAF_PasswordIn: "e10adc3949ba59abbe56e057f20f883e",
+      //FI_IP: "10.60.102.133"
+    }
+
+    
+
+    this.httpClient.post("http://localhost:2898/WsAfiliados.asmx?op=AfiliadosGetByNombre",postData,httpOptions )
+    .subscribe(data => {
+      console.log(data['_body']);
+     }, error => {
+      console.log(error);
+    });
+  }
+
 
 //Mensaje al usuario al no introducir su nombre o contraseña
   presentToast() {
     let toast = this.toastCtrl.create({
-      message: 'Por favor, introduzca su nombre de usuario y clave para continuar',
+      //message: 'Por favor, introduzca su nombre de usuario y clave para continuar',
+      message:this.user,
       duration: 3000,
       position: 'botton'
   });
   toast.onDidDismiss(() => {
-    console.log('Dismissed toast');
+    console.log(this.user);
   });
   toast.present();
 }
@@ -35,6 +81,7 @@ export class LoginPage {
     if (!params) params = {};
       if (this.credentialsForm.valid) //QUITARLE EL ! A LA VALIDACIÓN PARA QUE SIRVA
       {
+
        this.navCtrl.setRoot(WelcomePage);
       }
       else{
@@ -42,10 +89,14 @@ export class LoginPage {
       }
   }
 
+
 //Forma donde se valida el nombre y la contraseña del usuario
   credentialsForm = this.formBuilder.group({
-      name: ['jlaroche',Validators.required],
-      password:   ['', [Validators.required, Validators.minLength(5)]]
-    });
-
+      name: new FormControl('',Validators.compose([
+        //UsernameValidator.validUsername,
+        Validators.required
+      ])),
+      password: new FormControl('',Validators.required)
+    })
+  
 }
