eab10b246a67e561016be26e80489cb4a4af8233
Aveledo <Jonathan Aveledo@fondocomun.com.ve>
2019-01-25 13:59:17 -0400

    Avances parseando el xml de login. Xml de la rafaga de la respuesta

--- bancamoviljuridica/src/pages/login/login.ts
+++ bancamoviljuridica/src/pages/login/login.ts
@@ -8,12 +8,13 @@ import { WelcomePage } from '../welcome/welcome';
 import { LoginProvider } from '../../providers/login/login';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import 'rxjs/add/operator/map';
-import xml2js from 'xml2js';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Http, Headers, RequestOptions } from '@angular/http';
-
+import jsSHA from 'jssha'
+import {Md5} from 'ts-md5/dist/md5';
+import * as x2js  from 'xml2json';
 
 
 
@@ -27,15 +28,16 @@ export class LoginPage {
   user:string;
   nombre:string;
   contra:string;
+  contra1:string|Int32Array;
+  rafaga:string='Ingrese nombre de usuario y contraseña';
 
   constructor(public navCtrl: NavController,  private formBuilder: FormBuilder, 
     private toastCtrl: ToastController, public LoginProvider: LoginProvider,public httpClient: HttpClient ) {
-      
+    
+  
   }
 
-
-
- /* sendPostRequest() {
+/* sendPostRequest() {
     var headers = new HttpHeaders();
     headers.append('Content-Type', 'application/x-www-form-urlencoded');
     const httpOptions = {
@@ -61,15 +63,13 @@ export class LoginPage {
     });
   }*/
 
-
   
-
 
 //Mensaje al usuario al no introducir su nombre o contraseña
   presentToast() {
     let toast = this.toastCtrl.create({
       //message: 'Por favor, introduzca su nombre de usuario y clave para continuar',
-      message:this.user,
+      message:this.rafaga,
       duration: 3000,
       position: 'botton'
   });
@@ -84,13 +84,11 @@ export class LoginPage {
       if (this.credentialsForm.valid) //QUITARLE EL ! A LA VALIDACIÓN PARA QUE SIRVA
       {
         this.sendPostRequest();
-        this.navCtrl.setRoot(WelcomePage);
       }
       else{
      this.presentToast();
       }
   }
-
 
 //Forma donde se valida el nombre y la contraseña del usuario
   credentialsForm = this.formBuilder.group({
@@ -102,6 +100,8 @@ export class LoginPage {
     })
   
     sendPostRequest() {
+      this.contra1 = Md5.hashStr(this.contra);
+      console.log(this.contra1);
       var headers = new HttpHeaders();
       headers.append('Content-Type', 'text/xml');
       const httpOptions = {
@@ -115,7 +115,7 @@ export class LoginPage {
       <soap12:Body>
         <AfiliadosLogin xmlns="http://tempuri.org/">
           <sAF_NombreUsuarioIn>`+this.nombre+`</sAF_NombreUsuarioIn>
-          <sAF_PasswordIn>`+this.contra+`</sAF_PasswordIn>
+          <sAF_PasswordIn>`+this.contra1+`</sAF_PasswordIn>
           <FI_IP>10.60.102.100</FI_IP>
         </AfiliadosLogin>
       </soap12:Body>
@@ -125,15 +125,56 @@ export class LoginPage {
       //[EnableCors(origins: "http://mywebclient.azurewebsites.net", headers: "*", methods: "*")]
       this.httpClient.post("http://localhost:2898/WsAfiliados.asmx?op=AfiliadosGetByNombre",postData,httpOptions )
       .subscribe(data => {
-        console.log(data['_body']);
+        console.log('Data: '+data['_body']);
        }, error => {
-        console.log(error);
+        console.log('Error: '+JSON.stringify(error));
+        var str = JSON.stringify(error);
+        console.log("stingified: ", str);
+
+        var search_array = JSON.parse(str);
+        //var result = search_array[1].error;
+        console.log("result: ", search_array.error.text);
+        
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(search_array.error.text, "application/xml");
+        console.log(doc)
+
+        let c = doc;
+        let myJson = x2js.xml_str2json(c);
+        console.log(myJson);
+
+ 
+
+        //var search_array = JSON.parse(str);
+        //var parseString = require('xml2js').parseString;
+        //var xml = "<root>Hello xml2js!</root>"
+       // parseString(doc, function (err, result) {
+        //console.log(result);
+        //});
+       /* var cleanedString = doc.require("\ufeff", "");
+        var convert = require('xml-to-json-promise');
+        convert.xmlDataToJSON(cleanedString).then(json => {
+          console.log(json);
+          });*/
+
+         /* xml2js.parseString(doc.toString(), function (err, result) {
+            console.log('ACA FUNCIONA:'+result);
+          });
+        */
+
+
+        //this.rafaga = JSON.parse(error.Body);
+        //this.rafaga= JSON.parse(this.rafaga);
+        console.log(this.rafaga);
+        
+        this.presentToast();
+        //ACA TENDREMOS QUE VALIDAR QUE LA RÁFAGA ESTÉ CORRECTA, SI LO ESTÁ PASAMOS AL
+        //SET ROOT, SI NO HACEMOS UN TOAST Y LO DEJAMOS AHI
+        this.navCtrl.setRoot(WelcomePage);
       });
     }
 
 }
-
-
 
 
 
